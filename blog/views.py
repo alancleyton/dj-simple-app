338a424248754fa760fa0154a6c80f9eea91cf95
Alan Cleyton <alancleyton67@gmail.com>
2024-05-28 20:41:41 -0300

    add blog articles view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def articles(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return HttpResponse('blog articles')
