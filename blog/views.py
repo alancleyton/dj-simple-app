18470d9859bc8c58c0c6ea085ab59a083b558044
Alan Cleyton <alancleyton67@gmail.com>
2024-06-03 19:30:55 -0300

    send data to template with context

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,7 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 def articles(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
-        return HttpResponse(render(request, 'articles/index.html'))
+        return HttpResponse(render(request, 'articles/index.html', {
+            'header_title': 'Articles - list of articles ',
+            'title': 'Articles page'
+        }))
