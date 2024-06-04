0fede42d3145c43f6bb6ec6e6af9862f7092d511
Alan Cleyton <alancleyton67@gmail.com>
2024-06-04 11:17:57 -0300

    fix blog views code indentation

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 def articles(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
-        return HttpResponse(render(request, 'articles/index.html', {
-            'header_title': 'Articles - list of articles ',
-            'title': 'Articles page'
-        }))
+        return HttpResponse(render(
+                request, 'articles/index.html', 
+                { 'head_title': 'Blog - list of articles' }))
