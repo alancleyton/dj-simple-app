2664691d8660e8446fd2d356950f870413ffc4a4
Alan Cleyton <alancleyton67@gmail.com>
2024-06-06 12:12:08 -0300

    add data to blog views

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from blog.data import posts
 
 # Create your views here.
 def articles(request: HttpRequest) -> HttpResponse:
+    context = {
+        'head_title': 'Blog - Articles',
+        'posts': posts,
+    }
+
     if request.method == 'GET':
-        return HttpResponse(render(
-                request, 'blog/articles.html', 
-                { 'head_title': 'Blog - list of articles' }))
+        return HttpResponse(render(request, 'blog/articles.html', context ))
 
 def example(request: HttpRequest) -> HttpResponse:
+    context = {
+        'head_title': 'Blog - Articles',
+    }
+
     if request.method == 'GET':
-        return HttpResponse(render(
-                request, 'blog/example.html', 
-                { 'head_title': 'Blog - example page' }))
+        return HttpResponse(render(request, 'blog/example.html', context ))
